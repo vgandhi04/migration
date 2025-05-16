c542eb387b00b52df7aaf03b9bcb416923356d91
Vivek Gandhi <50017034+vgandhi04@users.noreply.github.com>
2025-05-16 11:35:18 +0530

    Add files via upload

---
+++ demo_ai.py
@@ -0,0 +1,38 @@
+import anthropic
+
+client = anthropic.Anthropic(
+  # defaults to os.environ.get("ANTHROPIC_API_KEY")
+  api_key="<API Key>",
+)
+
+message_batch = client.messages.batches.create(
+    requests=[
+        {
+            "custom_id": "first-prompt-in-my-batch",
+            "params": {
+                "model": "claude-3-5-haiku-20241022",
+                "max_tokens": 100,
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": "Hey Claude, tell me a short fun fact about video games!",
+                    }
+                ],
+            },
+        },
+        {
+            "custom_id": "second-prompt-in-my-batch",
+            "params": {
+                "model": "claude-3-7-sonnet-20250219",
+                "max_tokens": 100,
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": "Hey Claude, tell me a short fun fact about bees!",
+                    }
+                ],
+            },
+        },
+    ]
+)
+print(message_batch)
